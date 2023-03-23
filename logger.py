4d50fc83dc8d3b9f56a38bdc6c6073f39aefe337
VargasJuanP <92010526+VargasJuanP@users.noreply.github.com>
2023-03-22 22:47:16 -0500

    Implement logger class for logging events and errors

---
+++ logger.py
@@ -0,0 +1,38 @@
+import logging
+
+class Logger:
+
+    def __init__(self, log_file: str) -> None:
+        self.__logger = self.__create_logger(log_file)
+
+    def __create_logger(self, log_file: str) -> logging.Logger:
+
+        logger = logging.getLogger()
+        logger.setLevel(logging.INFO)
+        
+        file_handler = logging.FileHandler(log_file)
+        file_handler.setLevel(logging.DEBUG)
+        
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        file_handler.setFormatter(formatter)
+        
+        logger.addHandler(file_handler)
+
+        return logger
+    
+    def debug(self, message: str) -> None:
+        self.__logger.debug(message)
+        
+    def info(self, message: str) -> None:
+        self.__logger.info(message)
+        
+    def warning(self, message: str) -> None:
+        self.__logger.warning(message)
+        
+    def error(self, message: str) -> None:
+        self.__logger.error(message)
+        
+    def critical(self, message: str) -> None:
+        self.__logger.critical(message)
+
+logger = Logger("app.log")
