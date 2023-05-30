0f7f4ef0455ba2ac0cd73167f45d821168ab53cb
VargasJuanP <92010526+VargasJuanP@users.noreply.github.com>
2023-05-30 05:37:10 -0500

    Add files via upload

--- src/js/main.js
+++ src/js/main.js
@@ -1,18 +1,19 @@
 document.getElementById('loginForm').addEventListener('submit', async function(event) {
     event.preventDefault();
-  
+      
     let steamid = document.getElementById('steamid').value;
     let steamApiKey = document.getElementById('steam_api_key').value;
     let steamGridApiKey = document.getElementById('steam_grid_api_key').value;
-
-    //alert("Creando libreria, espere un momento...")
-
-    let data = await eel.crear_libreria({"steam_id": steamid, "steam_api_key": steamApiKey, "steam_grid_api_key": steamGridApiKey})();
     
+    var data = await eel.crear_libreria({"steam_id": steamid, "steam_api_key": steamApiKey, "steam_grid_api_key": steamGridApiKey})();
+
     if (data["response"] == "success") {
+        localStorage.setItem("steam_id", steamid)
+        localStorage.setItem("steam_api_key", steam_api_key)
+        localStorage.setItem("steam_grid_api_key", steam_grid_api_key)
         window.location.href = './index.html'; // Redirige a la vista index
     } else {
         alert(data["response"]);
     }
-
-  });
+    
+});
